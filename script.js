9b59d1b76bbb27eca69b7b1b42ccd48da13c7b24
Maëlle Aucher <maelle.aucher@gmail.com>
2025-12-10 10:14:04 +0100

    step 1

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// A toi de jouer pour cette partie :-) Happy coding !
+const btn = document.querySelector("button");
+const input = document.querySelector("#cityInput");
+const gpsDiv = document.querySelector("#gps");
+const cityTitle = document.querySelector("#city");
+
+btn.addEventListener("click", async () => {
+
+    const cityName = input.value.trim();
+    if (cityName === "") return;
+
+    try {
+        const response = await fetch(`https://nominatim.openstreetmap.org/search?city=${cityName}&format=json&limit=1`);
+
+        const data = await response.json();
+
+        if (data.length === 0) {
+            cityTitle.textContent = "Erreur";
+            gpsDiv.textContent = "Impossible de trouver la ville";
+            return;
+        }
+
+        const { lat, lon, display_name } = data[0];
+
+        const cityShortName = display_name.split(",")[0].trim();
+        cityTitle.textContent = `${cityShortName} - Coordonnées GPS : ${lat}, ${lon}`;
+        gpsDiv.textContent = "";
+
+    } catch (error) {
+        console.error(error);
+        gpsDiv.textContent = "Erreur serveur";
+    }
+
+
+});
